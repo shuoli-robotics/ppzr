224723de82037e4aa0860de2f6c363445c590268
Shuo Li <ls90911@gmail.com>
2016-08-11 17:35:37 +0200

    creat 'guidance_loop_velocity_autonomous_race'module and compiled successfully

---
+++ sw/airborne/modules/guidance_loop_velocity_autonomous_race/guidance_loop_velocity_autonomous_race.c
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) Shuo Li
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/guidance_loop_velocity_autonomous_race/guidance_loop_velocity_autonomous_race.c"
+ * @author Shuo Li
+ * This module is used to control velocity only in MODULE mode
+ */
+
+#include "modules/guidance_loop_velocity_autonomous_race/guidance_loop_velocity_autonomous_race.h"
+#include "state.h"
+#include "firmwares/rotorcraft/autopilot.h"
+#include "firmwares/rotorcraft/guidance/guidance_h.h"
+#include "firmwares/rotorcraft/stabilization/stabilization_attitude_euler_float.h"
+#include "firmwares/rotorcraft/stabilization.h"
+
+
+#define CMD_OF_SAT  1500 // 40 deg = 2859.1851
+
+#ifndef VISION_PHI_PGAIN
+#define VISION_PHI_PGAIN 400
+#endif
+PRINT_CONFIG_VAR(VISION_PHI_PGAIN)
+
+#ifndef VISION_PHI_IGAIN
+#define VISION_PHI_IGAIN 20
+#endif
+PRINT_CONFIG_VAR(VISION_PHI_IGAIN)
+
+#ifndef VISION_THETA_PGAIN
+#define VISION_THETA_PGAIN 400
+#endif
+PRINT_CONFIG_VAR(VISION_THETA_PGAIN)
+
+#ifndef VISION_THETA_IGAIN
+#define VISION_THETA_IGAIN 20
+#endif
+PRINT_CONFIG_VAR(VISION_THETA_IGAIN)
+
+#ifndef VISION_DESIRED_VX
+#define VISION_DESIRED_VX 0
+#endif
+PRINT_CONFIG_VAR(VISION_DESIRED_VX)
+
+#ifndef VISION_DESIRED_VY
+#define VISION_DESIRED_VY 0
+#endif
+PRINT_CONFIG_VAR(VISION_DESIRED_VY)
+
+struct guidance_module_st guidance_module = {
+        .phi_pgain = VISION_PHI_PGAIN,
+        .phi_igain = VISION_PHI_IGAIN,
+        .theta_pgain = VISION_THETA_PGAIN,
+        .theta_igain = VISION_THETA_IGAIN,
+        .desired_vx = VISION_DESIRED_VX,
+        .desired_vy = VISION_DESIRED_VY
+};
+
+void guidance_loop_vel_init() {
+    guidance_module.err_vx_int = 0;
+    guidance_module.err_vy_int = 0;
+    guidance_module.cmd.phi = 0;
+    guidance_module.cmd.theta = 0;
+    guidance_module.cmd.psi = stateGetNedToBodyEulers_i()->psi;
+}
+
+void guidance_h_module_run(bool in_flight)
+{
+    /* Update the setpoint */
+    //stabilization_attitude_set_rpy_setpoint_i(&guidance_module.cmd);
+
+    stab_att_sp_euler.phi = guidance_module.cmd.phi;
+    stab_att_sp_euler.theta = guidance_module.cmd.theta;
+    stab_att_sp_euler.psi = guidance_module.cmd.psi;
+
+    /* Run the default attitude stabilization */
+    stabilization_attitude_run(in_flight);
+}
+
+/** Thus function is used to calculate needed phi and theta
+ * @param vel_x is desired velocity in earth coordinates
+ * @param vel_y is desired velocity in earth coordinates
+ */
+void guidance_loop_pid()
+{
+    /* Check if we are in the correct AP_MODE before setting commands */
+    if (autopilot_mode != AP_MODE_MODULE) {
+        return;
+    }
+    float current_vel_x = stateGetSpeedNed_f()->x;
+    float current_vel_y = stateGetSpeedNed_f()->y;
+    /* Calculate the error */
+    float err_vx = guidance_module.desired_vx - current_vel_x;
+    float err_vy = guidance_module.desired_vy - current_vel_y;
+
+    /* Calculate the integrated errors (TODO: bound??) */
+    guidance_module.err_vx_int += err_vx / 512;
+    guidance_module.err_vy_int += err_vy / 512;
+
+    struct FloatVect2 cmd_f;
+    /* Calculate the commands */
+    cmd_f.x   = guidance_module.phi_pgain * err_vy
+                               + guidance_module.phi_igain * guidance_module.err_vy_int;
+    cmd_f.y   = -(guidance_module.theta_pgain * err_vx
+                                 + guidance_module.theta_igain * guidance_module.err_vx_int);
+    float psi = stateGetNedToBodyEulers_f()->psi;
+    float s_psi = sinf(psi);
+    float c_psi = cosf(psi);
+    guidance_module.cmd.phi = -s_psi * cmd_f.x + c_psi * cmd_f.y;
+    guidance_module.cmd.theta = -c_psi * cmd_f.x - s_psi * cmd_f.y;
+    /* Bound the roll and pitch commands */
+    BoundAbs(guidance_module.cmd.phi, CMD_OF_SAT);
+    BoundAbs(guidance_module.cmd.theta, CMD_OF_SAT);
+}
+
+void guidance_loop_set_heading(float heading){
+    guidance_module.cmd.psi = heading;
+}
+
+void guidance_loop_set_velocity(float vx, float vy){
+    guidance_module.desired_vx = vx;
+    guidance_module.desired_vy = vy;
+}
---
+++ sw/airborne/modules/guidance_loop_velocity_autonomous_race/guidance_loop_velocity_autonomous_race.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) Shuo Li
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/guidance_loop_velocity_autonomous_race/guidance_loop_velocity_autonomous_race.h"
+ * @author Shuo Li
+ * This module is used to control velocity only in MODULE mode
+ */
+
+#ifndef GUIDANCE_LOOP_VELOCITY_AUTONOMOUS_RACE_H
+#define GUIDANCE_LOOP_VELOCITY_AUTONOMOUS_RACE_H
+
+#include "math/pprz_algebra_int.h"
+
+struct guidance_module_st {
+    int32_t phi_pgain;        ///< The roll P gain on the err_vx
+    int32_t phi_igain;        ///< The roll I gain on the err_vx_int
+    int32_t theta_pgain;      ///< The pitch P gain on the err_vy
+    int32_t theta_igain;      ///< The pitch I gain on the err_vy_int
+    float desired_vx;         ///< The desired velocity in the x direction (cm/s)
+    float desired_vy;         ///< The desired velocity in the y direction (cm/s)
+
+    float err_vx_int;         ///< The integrated velocity error in x direction (m/s)
+    float err_vy_int;         ///< The integrated velocity error in y direction (m/s)
+    struct Int32Eulers cmd;   ///< The commands that are send to the hover loop
+};
+
+extern struct guidance_module_st guidance_module;
+
+extern void guidance_loop_vel_init(void);
+extern void guidance_loop_pid(void);
+extern void guidance_loop_set_heading(float heading);
+extern void guidance_loop_set_velocity(float vx, float vy);
+extern void guidance_h_module_run(bool in_flight);
+#endif
+
