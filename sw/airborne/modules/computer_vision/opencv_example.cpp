c8a44e812b3e51666834d9cb00958eba6595b152
Roland Meertens <rolandmeertens@gmail.com>
2016-08-19 11:46:38 +0200

    cleaned up

--- sw/airborne/modules/computer_vision/opencv_example.cpp
+++ sw/airborne/modules/computer_vision/opencv_example.cpp
@@ -53,15 +53,6 @@ Mat image_color, yuvimage, Z;
 uint8_t only_uv_u_lookup[256];
 uint8_t only_uv_v_lookup[256];
 uint8_t h_lookup[256];
-static unsigned int get_area_in_border(Mat image, int x, int y,
-		int width_heigth) {
-	Point right_under(y + width_heigth, x + width_heigth);
-	Point right_up(y, x + width_heigth);
-	Point left_under(y + width_heigth, x);
-	Point left_up(y, x);
-	return image.at<int>(right_under) - image.at<int>(right_up)
-			- image.at<int>(left_under) + image.at<int>(left_up);
-}
 
 inline void start_clock() {
 	begin = clock();
@@ -73,7 +64,7 @@ inline void end_clock(char *name_part) {
 	printf("%s took %f seconds\n", name_part, elapsed_secs);
 }
 
-void init_lookup_table() {
+static void init_lookup_table() {
 
 	int max=256/2;
 	int meanu2=1255;
@@ -103,7 +94,7 @@ void opencv_init_rects() {
 	init_lookup_table();
 }
 
-void yuv422_set_color_intensity(Mat colorIntensity,char* img) {
+static void yuv422_set_color_intensity(Mat colorIntensity,char* img) {
 	int n_rows = colorIntensity.rows;
 	int n_cols = colorIntensity.cols;
 
@@ -142,7 +133,7 @@ void yuv422_set_color_intensity(Mat colorIntensity,char* img) {
 }
 
 
-void hsv_set_color_intensity(Mat colorIntensity,Mat hsvImage) {
+static void hsv_set_color_intensity(Mat colorIntensity,Mat hsvImage) {
 	int n_rows = colorIntensity.rows;
 	int n_cols = colorIntensity.cols;
 
@@ -168,7 +159,7 @@ void hsv_set_color_intensity(Mat colorIntensity,Mat hsvImage) {
 
 }
 
-void smooth_array(float array[],int length_array, int smooth_dist ){
+static void smooth_array(float array[],int length_array, int smooth_dist ){
 		for (int j = length_array-1;j>smooth_dist; j--) {
 			int sum=0;
 			for(int x=0;x<smooth_dist;x++){
@@ -177,80 +168,7 @@ void smooth_array(float array[],int length_array, int smooth_dist ){
 			array[j]=sum/float(smooth_dist);
 		}
 }
-void grayscale_hor_sum(Mat grayImage, uint32_t hor_sum[],uint32_t vert_sum[]){
-	int n_rows = grayImage.rows;
-	int n_cols = grayImage.cols;
 
-	// Iterate over the image,
-	int i, j;
-	uchar *p;
-	int index_img = 0;
-	int bin_divider=4;
-
-	// TODO Memset
-	for (j = 0; j < n_cols/bin_divider; j ++) {
-		hor_sum[j]=0;
-	}
-
-	for (j = 0; j < n_rows/bin_divider; j ++) {
-		vert_sum[j]=0;
-	}
-
-	// set each sum
-	for (i = 0; i < n_rows; ++i) {
-		p = grayImage.ptr<uchar>(i);
-		for (j = 0; j < n_cols; j++) {
-			hor_sum[j/bin_divider]+=p[j];
-			vert_sum[i/bin_divider]+=p[j];
-		}
-	}
-
-	// Now smooth!
-	int smooth_dist=5;
-	for (j = n_cols/bin_divider;j>smooth_dist; j--) {
-		int sum=0;
-		for(int x=0;x<smooth_dist;x++){
-			sum+=hor_sum[j-x];
-		}
-		hor_sum[j]=sum;
-	}
-
-	for (j = n_rows/bin_divider;j>smooth_dist; j--) {
-		int sum=0;
-		for(int x=0;x<smooth_dist;x++){
-			sum+=vert_sum[j-x];
-		}
-		vert_sum[j]=sum;
-	}
-
-	int maxLocation2=0;
-	uint32_t maxValue2=0;
-	int maxLocation_vert=0;
-	int max_value_vert=0;
-	for (j = 0; j < n_rows/bin_divider; j ++) {
-		if(vert_sum[j]>max_value_vert){
-			max_value_vert=vert_sum[j];
-			maxLocation_vert = j;
-		}
-	}
-	for (j = 0; j < n_cols/bin_divider; j ++) {
-		if(hor_sum[j]>maxValue2){
-			maxValue2=hor_sum[j];
-			maxLocation2 = j;
-		}
-
-	}
-
-//	line(grayImage,Point(0,maxLocation*bin_divider),Point(n_rows,maxLocation*bin_divider),Scalar(255,255,255),5);
-
-	//line(grayImage,Point(maxLocation2*bin_divider,0),Point(maxLocation2*bin_divider,n_rows),Scalar(255,255,255),5);
-	loc_y=maxLocation_vert*bin_divider;
-	line(grayImage,Point(0,maxLocation_vert*bin_divider),Point(n_cols,maxLocation_vert*bin_divider),Scalar(255,255,255),5);
-
-	//	C++: void line(Mat& img, Point pt1, Point pt2, const Scalar& color, int thickness=1, int lineType=8, int shift=0)
-
-	printf("%d %d\n",maxLocation2,maxValue2);
-}
 bool changedParameters(){
 	if(prev_mean_u!=mean_u || prev_mean_v != mean_v || prev_stddev_colors != stddev_colors){
 		prev_mean_u = mean_u;
@@ -492,10 +410,6 @@ int opencv_example(char *img, int width, int height) {
 
 	//	yuv422_set_color_intensity(probImage,img);
 	guidoMethod(probImage);
-//	uint32_t hor_sum_image[width];
-//	uint32_t vert_sum_image[height];
-
-	//grayscale_hor_sum( probImage,hor_sum_image,vert_sum_image);
 	grayscale_opencv_to_yuv422(probImage, img, width, height);
 	return 0;
 }
