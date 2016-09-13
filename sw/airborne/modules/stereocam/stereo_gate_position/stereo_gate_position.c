c2c9c9072f68c7724cc8111c498d196a99112c30
mmoiozo <ozo1994@live.nl>
2016-09-13 17:34:43 +0200

    z gate position added

--- sw/airborne/modules/stereocam/stereo_gate_position/stereo_gate_position.c
+++ sw/airborne/modules/stereocam/stereo_gate_position/stereo_gate_position.c
@@ -10,20 +10,39 @@
  * Stereo board gate detection is fused with optic flow velocity.
  */
 
+#include <math.h>
 #include "subsystems/datalink/telemetry.h"
 #include "modules/stereocam/stereo_gate_position/stereo_gate_position.h"
 
+
+#define PI 3.1415926
+
+
 void stereocam_to_state(void);
 
-char x_center = 1;
+char x_center = 0;
 char y_center = 0;
 char radius   = 0;
 char fitness  = 0;
-char fps      = 1;
+char fps      = 0;
+
+float measured_x_gate = 0;
+float measured_y_gate = 0;
+float measured_z_gate = 0;
+
+// Settings:
+float FOV_width = 57.4f;
+float FOV_height = 44.5f;
+float gate_size_meters = 1.0f;
+
+float deg2rad(float deg)
+{
+	return ((deg * PI) / 180.0f);
+}
 
 static void stereo_gate_send(struct transport_tx *trans, struct link_device *dev)
     {
-    pprz_msg_send_STEREO_GATE_INFO(trans, dev, AC_ID,&x_center, &y_center,&radius,&fitness,&fps);
+    pprz_msg_send_STEREO_GATE_INFO(trans, dev, AC_ID,&x_center, &y_center,&radius,&fitness,&fps,&measured_x_gate,&measured_y_gate,&measured_z_gate);
     }  
 
  void stereo_gate_position_init(void)
@@ -39,6 +58,8 @@ static void stereo_gate_send(struct transport_tx *trans, struct link_device *dev
     stereocam_data.fresh = 0;
   }
  }
+ 
+
 
 void stereocam_to_state(void)
 {
@@ -50,6 +71,16 @@ void stereocam_to_state(void)
   radius   = stereocam_data.data[2];
   fitness  = stereocam_data.data[3];
   fps      = stereocam_data.data[4];
+  
+  // Determine the measurement:
+	float alpha = (radius / 128.0f) * FOV_width;
+	float measured_distance_gate = (0.5f * gate_size_meters) / tan(deg2rad(alpha));
+	float measured_angle_gate = ((x_center - 64.0f) / (128.0f)) * FOV_width;
+	float measured_angle_vert = ((y_center - 48.0f) / (96.0f)) * FOV_height;
+	
+	measured_x_gate = measured_distance_gate * sin(deg2rad(measured_angle_gate));
+	measured_y_gate = measured_distance_gate * cos(deg2rad(measured_angle_gate));
+	measured_z_gate = measured_distance_gate * sin(deg2rad(measured_angle_vert));
   
 }
 
