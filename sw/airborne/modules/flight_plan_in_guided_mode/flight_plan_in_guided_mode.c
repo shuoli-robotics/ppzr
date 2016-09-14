2665bb0b9eeee4dbf3331a87888705db56289133
Shuo Li <ls90911@gmail.com>
2016-09-14 14:30:51 +0200

    add y, not test

--- sw/airborne/modules/flight_plan_in_guided_mode/flight_plan_in_guided_mode.c
+++ sw/airborne/modules/flight_plan_in_guided_mode/flight_plan_in_guided_mode.c
@@ -50,6 +50,10 @@ float vx_earth;
 float vy_earth;
 float psi;
 float heading;
+float velocity_body_x;
+float velocity_body_y;
+float velocity_earth_x;
+float velocity_earth_y;
 bool altitude_is_arrived;
 bool adjust_position_mask;
 int primitive_in_use; // This variable is used for showing which primitive is used now;
@@ -221,8 +225,9 @@ void go_up_down(float derta_altitude){
 }
 
 void adjust_position(float derta_altitude){
-    // set altitude first
-    if (1)
+
+    // set z
+    if (adjust_position_mask == 0)
     {
         guidance_h_mode_changed(GUIDANCE_H_MODE_MODULE);
         guidance_v_mode_changed(GUIDANCE_V_MODE_GUIDED);
@@ -231,11 +236,23 @@ void adjust_position(float derta_altitude){
         psi0 = stateGetNedToBodyEulers_f()->psi;
         adjust_position_mask = 1;
     }
+    // set vx and vy
+    if (fabs(current_x_gate)<0.2)
+        velocity_body_y = 0;
+    else if (current_x_gate>0.2)
+        velocity_body_y = 0.2;
+    else if (current_x_gate<-0.2)
+        velocity_body_y = -0.2;
 
-    if (fabs(measured_x_gate)<0.2)
-        go_left_right(0);
-    else if (measured_x_gate>0.2)
-        go_left_right(0.2);
-    else if (measured_x_gate<-0.2)
-        go_left_right(-0.2);
+    if (fabs(current_y_gate-1.2)<0.2)
+        velocity_body_x = 0;
+    else if (current_y_gate-1.2>0.2)
+        velocity_body_x = 0.2;
+    else if (current_y_gate-1.2<-0.2)
+        velocity_body_x = -0.2;
+
+    velocity_earth_x = cosf(psi0)*velocity_body_x - sinf(psi0)*velocity_body_y;
+    velocity_earth_y = sinf(psi0)*velocity_body_x + cosf(psi0)*velocity_body_y;
+
+    guidance_loop_set_velocity(velocity_earth_x,velocity_earth_y);
 }
