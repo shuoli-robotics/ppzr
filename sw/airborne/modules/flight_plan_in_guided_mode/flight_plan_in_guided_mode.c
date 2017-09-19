c34c1167cc645c66be1c2f41b3d4e7337120321a
ls90911 <ls90911@gmail.com>
2017-09-19 11:09:08 +0200

    aa

--- sw/airborne/modules/flight_plan_in_guided_mode/flight_plan_in_guided_mode.c
+++ sw/airborne/modules/flight_plan_in_guided_mode/flight_plan_in_guided_mode.c
@@ -251,6 +251,8 @@ bool take_off(void)
 				tf_status.ave_altitude = 0.0;
 				guidance_v_mode_changed(GUIDANCE_V_MODE_HOVER);  // vertical module should be called!
 				set_altitude(TAKE_OFF_ALTITUDE);
+				// !!!!!!!!!!!!!!!!!!!!!!!!!!
+				race_state.flag_in_open_loop = TURE;
 
 		}
 
