685b3acbb88fde5f8ad79b88d1252a361ebe131a
Shuo Li (Thinkpad) <ls90911@gmail.com>
2018-04-13 14:20:21 +0200

    msg bug fixed hopefully

---
+++ cmake-build-debug/CMakeFiles/paparazzi.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/paparazzi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/paparazzi.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for paparazzi.
+# This may be replaced when dependencies are built.
